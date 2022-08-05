292f9266ffe902a29e290165b3e7b723c047168d
Anastasia Chumakova <pospelowa.anast@yandex.ru>
2022-08-05 18:24:48 +0300

    добавлена камера, но есть задержка

--- client.py
+++ client.py
@@ -1,10 +1,34 @@
+import base64
+
+import cv2
+import numpy as np
 import zmq
+
 context = zmq.Context()
 print("Connecting to hello world server")
 socket = context.socket(zmq.SUB)
 socket.connect("tcp://localhost:5555")
-while True:
-    print("Sending request %s")
-    cb, hb = socket.recv_multipart()
+socket.setsockopt_string(zmq.SUBSCRIBE, '')
 
-    print(f"Received reply {cb.decode('utf-8'), hb.decode('utf-8')}")
+
+while True:
+    try:
+        print("Sending request %s")
+        cb, hb = socket.recv_multipart()
+
+        print(f"Received reply {cb.decode('utf-8'), hb.decode('utf-8')}")
+        image_string = socket.recv_string()
+        # декодирует переданный файл
+        raw_image = base64.b64decode(image_string)
+        # Интерпретирует буфер как одномерный массив
+        image = np.frombuffer(raw_image, dtype=np.uint8)
+        # Считывает изображение из буфера
+        frame = cv2.imdecode(image, 1)
+        # Добавление изображения в окно
+        cv2.imshow("frame", frame)
+        # Остановка изображения от быстрого исчезновения
+        cv2.waitKey(1)
+    except KeyboardInterrupt:
+        # Закрывает окна
+        cv2.destroyAllWindows()
+        break
--- server.py
+++ server.py
@@ -1,18 +1,38 @@
-import time
+import base64
 import random
+import time
+
+import cv2
 import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind("tcp://*:5555")
+# Открывает камеру для захвата видео
+# В параметрах передается id  устройства (для работы по умолчанию 0)
+camera = cv2.VideoCapture(0)
 
 while True:
-    #  Wait for next request from client
+    try:
+        #  Wait for next request from client
 
-    #  Do some 'work'
-    time.sleep(1)
-
-    #  Send reply back to client
-    message = ', '.join(map(str, random.sample(range(10, 30), 5)))
-    print(message.encode('utf-8'))
-    socket.send_multipart([b"", message.encode('utf-8')])
+        #  Do some 'work'
+        time.sleep(1)
+        #  Send reply back to client
+        message = ', '.join(map(str, random.sample(range(10, 30), 5)))
+        print(message.encode('utf-8'))
+        socket.send_multipart([b"", message.encode('utf-8')])
+        # Захватывает, декодирует и возвращает кадр
+        ret, frame = camera.read()
+        frame = cv2.resize(frame, (640, 480))
+        # Кодирует изображение в буфер памяти
+        encoded, buf = cv2.imencode('.jpg', frame)
+        # Кодирование
+        image = base64.b64encode(buf)
+        socket.send(image)
+    except KeyboardInterrupt:
+        # Закрывает видеофайл или устройство захвата
+        camera.release()
+        # Закрывает окна
+        cv2.destroyAllWindows()
+        break
